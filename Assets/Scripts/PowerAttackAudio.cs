61c51ca2426000319db4b3787106c2d6f87a0858
Ricardo Morales <ricsf2000@gmail.com>
2025-12-04 07:33:05 -0600

    fix power slash sfx

--- Assets/Scripts/PowerAttackAudio.cs
+++ Assets/Scripts/PowerAttackAudio.cs
@@ -4,12 +4,13 @@ public class PowerAttackAudio : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip swordAttackClip;
+    [SerializeField] private float volume = 0.5f;
 
     public void PlaySwordAttack()
     {
         if (audioSource != null && swordAttackClip != null)
         {
-            audioSource.PlayOneShot(swordAttackClip);
+            audioSource.PlayOneShot(swordAttackClip, volume);
         }
     }
 }
